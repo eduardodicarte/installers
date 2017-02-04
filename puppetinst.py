3dedeb78f0f33e77bad3beeb8e05acaa4efa9f8c
root <root@localhost.localdomain>
2017-02-04 21:34:08 -0200

    Inclusão de repositorio. Correcao na configuracao da variavel de ambiente

--- puppetinst.py
+++ puppetinst.py
@@ -4,7 +4,9 @@
 import subprocess
 
 class PuppetInst:
-	puppetrep = "https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm"
+	puppetrep = "https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm"	
+	yumrepo   = "epel-release"	
+	
 	codesucess = 0
    
 	def __init__(self):
@@ -12,13 +14,14 @@ class PuppetInst:
 		self.do_install()
 
 	def install_repo(self):
+		subprocess.call("yum install " + self.yumrepo, shell = True)
 		subprocess.call("rpm -Uvh " + self.puppetrep, shell = True)
 
 	def do_install(self):
 		if subprocess.call("which puppet", shell = True) != self.codesucess:
 			subprocess.call("yum install puppet -y", shell = True)
 
-			subprocess.call("echo "\export PATH=/opt/puppetlabs/bin:$PATH\" > /etc/profile.d/puppet_path.sh",shell = True)
+			subprocess.call('echo "export PATH=/opt/puppetlabs/bin:$PATH" > /etc/profile.d/puppet_path.sh',shell = True)
 			subprocess.call("source /etc/profile.d/puppet_path.sh")
 
 			if subprocess.call("puppet --version", shell = True) == self.codesucess:

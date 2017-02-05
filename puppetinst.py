c1c3c94175fc2c61deaeea985d83305f2f4ba3c6
root <root@localhost.localdomain>
2017-02-05 19:50:58 -0200

    Remoção do tratamento de exception para debug

--- puppetinst.py
+++ puppetinst.py
@@ -7,7 +7,7 @@ class PuppetInst:
 	puppetrep = "http://yum.puppetlabs.com/el/7/PC1/x86_64/puppetlabs-release-pc1-0.9.2-1.el7.noarch.rpm"	
 	yumrepo   = "epel-release"	
 	
-	codesucess = 0
+	codesuccess = 0
    
 	def __init__(self):
 		self.install_repo()
@@ -18,13 +18,13 @@ class PuppetInst:
 		subprocess.call("rpm -Uvh " + self.puppetrep, shell = True)
 
 	def do_install(self):
-		if subprocess.call("which puppet", shell = True) != self.codesucess:
+		if subprocess.call("which puppet", shell = True) != self.codesuccess:
 			subprocess.call("yum install puppet -y --nogpgcheck", shell = True)
 
 			subprocess.call('echo "export PATH=/opt/puppetlabs/bin:$PATH" > /etc/profile.d/puppet_path.sh',shell = True)
 			subprocess.call("source /etc/profile.d/puppet_path.sh")
 
-			if subprocess.call("puppet --version", shell = True) == self.codesucess:
+			if subprocess.call("which puppet", shell = True) == self.codesuccess:
 				print "Instacao realizada com sucesso."
 			else:
 				print "Ocorreu um erro na instalacao. Contate o desenvolvedor do programa."
@@ -33,8 +33,5 @@ class PuppetInst:
 
 
 if __name__ == "__main__":
-	try:
-		PuppetInst()
-	except:
-		print "Ocorreu o erro na execucao da instalacao. Operacao mal sucedida."
+	PuppetInst()
 
